2db88cf6f6a065a8d7f9158865b1e4612e64f4d5
vbthang <2151062875@e.tlu.edu.vn>
2024-07-30 16:24:06 +0700

    feat: add rabbitmq

--- src/api/v1/dbs/rabbitmq.init.js
+++ src/api/v1/dbs/rabbitmq.init.js
@@ -1,20 +1,17 @@
 const amqp = require('amqplib')
 const { rabbitmq } = require('../../../config')
 
-let instance
+const getRabbitMQInstance = async () => {
+    const connection = await amqp.connect(rabbitmq)
+        
+    const channel = await connection.createChannel()
 
-const connect = async () => {
-    instance = await amqp.connect(rabbitmq)
-}
-
-connect()
-.then(() => console.log('\x1b[42m%s\x1b[0m', 'RabbitMQ: Connect successfully'))
-.catch(err => console.log('RabbitMQ: Connect failed ', err))
-
-const getInstanceMQ = async () => {
-    return instance
+    return {
+        connection,
+        channel
+    }
 }
 
 module.exports = {
-    getInstanceMQ
+    getRabbitMQInstance
 }
--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -4,6 +4,21 @@ const express = require('express')
 
 const router = express.Router()
 
+/////////// TEST MESSAGE QUEUE /////////////////
+router.post('/test', async (req, res, next) => {
+    // TEST MQ
+    const MessageQueue = require('../services/rabbitmq.service')
+    const msg = req.body
+    
+    await MessageQueue.send({
+        nameExchange: 'post_notification',
+        message: msg
+    })
+    res.json('Send OK')
+})
+/////////// END TEST MESSAGE QUEUE /////////////////
+
+
 // router.use('/api/v1/vote', require('./vote'))
 router.use('/api/v1/users', require('./user'))
 router.use('/api/v1/profile', require('./profile'))
