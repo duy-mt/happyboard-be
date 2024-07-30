6c8479432c9ab5ade5ee6ab2d40d1b0eb5161593
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-07-30 14:28:09 +0700

    Update rabbitmq.config.js

--- src/config/rabbitmq.config.js
+++ src/config/rabbitmq.config.js
@@ -1,6 +1,6 @@
 const dev = `amqp://${process.env.DEV_RMQ_USER}:${process.env.DEV_RMQ_PW}@${process.env.DEV_RMQ_HOST}`
 
-const prod = `amqp://${process.env.PROD_RMQ_USER}:${process.env.PROD_RMQ_PW}@${process.env.PROD_RMQ_HOST}`
+const prod = `amqp://guest:guest@172.31.33.195`
 
 
 const config = {
