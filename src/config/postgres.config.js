e1060c121349d24844a20499c82938cb41f90955
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-07-30 13:38:40 +0700

    Update postgres.config.js

--- src/config/postgres.config.js
+++ src/config/postgres.config.js
@@ -12,11 +12,11 @@ const dev = {
 }
 
 const prod = {
-    host: process.env.PROD_POSTGRES_HOST || 'localhost',
-    username: process.env.PROD_POSTGRES_USER || 'postgres',
-    password: process.env.PROD_POSTGRES_PW || 'rootpass',
-    database: process.env.PROD_POSTGRES_DB || 'hb',
-    port: process.env.PROD_POSTGRES_PORT || '5455',
+    host: process.env.PROD_POSTGRES_HOST || 'db-postgresql-sgp1-58474-do-user-15759758-0.f.db.ondigitalocean.com',
+    username: process.env.PROD_POSTGRES_USER || 'doadmin',
+    password: process.env.PROD_POSTGRES_PW || 'AVNS_b59T6z0uB8jwksrGte9',
+    database: process.env.PROD_POSTGRES_DB || 'defaultdb',
+    port: process.env.PROD_POSTGRES_PORT || 'defaultdb',
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
