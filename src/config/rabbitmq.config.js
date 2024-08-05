3de4ac6748828ec00b863f0d3e7c38bef7f99ba4
duymt <duy.mtdevhust@gmail.com>
2024-08-05 16:46:11 +0700

    update config && main.yml file

--- src/config/rabbitmq.config.js
+++ src/config/rabbitmq.config.js
@@ -1,4 +1,4 @@
-const dev = process.env.DEV_RMQ_PW ? `amqp://${process.env.DEV_RMQ_USER}:${process.env.DEV_RMQ_PW}@${process.env.DEV_RMQ_HOST}` : `amqps://vsdhodqk:hCS5TqgU9PLDub7jVf18NOkQTHegQ_Wk@armadillo.rmq.cloudamqp.com/vsdhodqk`
+const dev = process.env.DEV_RMQ_PW ? `amqp://${process.env.DEV_RMQ_USER}:${process.env.DEV_RMQ_PW}@${process.env.DEV_RMQ_HOST}` : `amqps://uypxmvvt:U8D0oQym0Bdm8DmH3iLvxT8ag6V-CBNA@octopus.rmq3.cloudamqp.com/uypxmvvt`
 
 const prod = `amqps://vsdhodqk:hCS5TqgU9PLDub7jVf18NOkQTHegQ_Wk@armadillo.rmq.cloudamqp.com/vsdhodqk`
 
