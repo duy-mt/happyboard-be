dd1bac202796307594de35fafa27afb090ba8300
duymt <duy.mtdevhust@gmail.com>
2024-08-06 11:08:55 +0700

    update redis config

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,7 +1,10 @@
-const dev = process.env.DEV_REDIS_URI ? process.env.DEV_REDIS_URI : "redis://172.31.33.195:6379"
+const dev = process.env.DEV_REDIS_URI ? process.env.DEV_REDIS_URI : `redis://default:YYo74ADDrdISfNU6ZTDxQyfdjBTSWTlX@redis-14543.c323.us-east-1-2.ec2.redns.redis-cloud.com:14543`
 
-const prod ="redis://172.31.33.195:6379"
-
+const prod = {
+    host: "redis-14543.c323.us-east-1-2.ec2.redns.redis-cloud.com",
+    port: "14543",
+    password: "YYo74ADDrdISfNU6ZTDxQyfdjBTSWTlX"
+}
 const config = {
     dev, prod
 }
