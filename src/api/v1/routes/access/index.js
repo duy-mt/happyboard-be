670a8f8fc213eb7acef3c56ea8bb74ea300a1887
vbthang <2151062875@e.tlu.edu.vn>
2024-08-15 08:54:12 +0700

    feat: forgot pw & send mail

--- src/api/v1/routes/access/index.js
+++ src/api/v1/routes/access/index.js
@@ -25,6 +25,9 @@ router.get('/auth/google/failure', (req, res, next) => {
 router.post('/signup', asyncHandler(accessController.signUp))
 router.post('/signin', asyncHandler(accessController.login))
 router.post('/refresh', asyncHandler(accessController.refreshToken))
+router.post('/forgot-password', asyncHandler(accessController.sendEmailForgotPW))
+router.get('/reset-password', asyncHandler(accessController.validateToken))
+router.post('/reset-password', asyncHandler(accessController.resetPW))
 
 router.use(asyncHandler(authentication))
 
