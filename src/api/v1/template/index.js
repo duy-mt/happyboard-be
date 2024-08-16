6e13e542980723b78bd5e7cb998341c2d163a541
vbthang <2151062875@e.tlu.edu.vn>
2024-08-16 13:53:26 +0700

    update: change url forgot pw

--- src/api/v1/template/index.js
+++ src/api/v1/template/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const htmlForgotPW = async ({
     email, token
 }) => {
-    let url = process.env.DOMAIN_RESET_PASSWORD + `?token=${token}`
+    let url = process.env.DOMAIN_RESET_PASSWORD + `/${token}`
     let data = await fs.readFileSync(path.resolve(__dirname, 'forgotPW.html'), 'utf8')
     data = data.replace('@@@url@@@', url)
     return data;
