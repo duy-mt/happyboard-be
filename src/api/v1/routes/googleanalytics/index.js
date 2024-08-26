16dca9aa8ad326d1ef0ccd30a9b3cc493ed33e14
duymt <duy.mtdevhust@gmail.com>
2024-08-26 13:30:05 +0700

    update filter idea && api GA

--- src/api/v1/routes/googleanalytics/index.js
+++ src/api/v1/routes/googleanalytics/index.js
@@ -10,8 +10,8 @@ const router = express.Router()
 
 router.use(asyncHandler(authentication))
 
-router.get('/views', authorize(['IDE02']), asyncHandler(GoogleAnalyticsController.getViewsByHour))
-router.get('/events', authorize(['IDE02']), asyncHandler(GoogleAnalyticsController.getEventsByHour))
+router.get('/views', authorize(['IDE02']), asyncHandler(GoogleAnalyticsController.getViewsByDay))
+router.get('/events', authorize(['IDE02']), asyncHandler(GoogleAnalyticsController.getEventsByDay))
 
 
 module.exports = router
